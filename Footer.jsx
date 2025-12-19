d3e8b055f213a1f8ad9c22fed17a274bb55e6b8a
Anurag2002git <anuragd158@gmail.com>
2025-12-19 20:11:30 +0530

    Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+    <footer className='text-center bg-gray-600 text-white h-10 mt-20 pt-10 flex justify-center items-center'>
+      <p>this is footer section..</p>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
